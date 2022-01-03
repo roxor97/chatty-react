574471b4471ee8944666ec67428beee199c75624
Roxor <junyperez97@gmail.com>
2022-01-03 18:27:46 -0500

    se completo la aplicacion

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,34 +1,20 @@
-import React from 'react';
-import 'firebase/auth'
-import { useFirebaseApp, useUser } from 'reactfire'
-import { useState } from 'react';
-import { getAuth } from 'firebase/auth';
+import { auth } from "../services/firebase-config";
 
-const Auth = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
 
-    const firebase = useFirebaseApp();
-
-    const handleSubmit = async () => {
-        await firebase.auth().createUserWithEmailAndPassword(email, password);
-    }
-
-    return (
-        <div>
-            <form className='m-5'>
-                <div className="mb-3">
-                    <label className="form-label">Correo electronico:</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" onChange={(event) => setEmail(event.target.value)} />
-                </div>
-                <div className="mb-3">
-                    <label className="form-label">Contraseña:</label>
-                    <input type="password" className="form-control" id="password" onChange={(event) => setPassword(event.target.value)} />
-                </div>
-                <button onClick={handleSubmit} type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </div>
-    )
+export function signup(email, password) {
+    return auth().createUserWithEmailAndPassword(email, password);
 }
 
-export default Auth
+export function signin(email, password) {
+    return auth().signInWithEmailAndPassword(email, password);
+}
+
+export function signInWithGoogle() {
+    const provider = new auth.GoogleAuthProvider();
+    return auth().signInWithPopup(provider);
+}
+
+export function signInWithGitHub() {
+    const provider = new auth.GithubAuthProvider();
+    return auth().signInWithPopup(provider);
+}
